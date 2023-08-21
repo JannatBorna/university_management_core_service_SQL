96f59b1f8eb268c462d376b4985b3fb7302ce485
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-08-21 20:31:29 +0600

    add

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidations } from './building.validation';
 
 const router = express.Router();
 
-router.post('/', BuildingController.insertIntoDB);
+// router.post('/', BuildingController.insertIntoDB);
 router.get('/', BuildingController.getAllFromDB);
+
+//validation
+router.post(
+  '/',
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN), // jwt token
+  validateRequest(BuildingValidations.create),
+  BuildingController.getAllFromDB
+);
 
 export const buildingRoutes = router;
