bcc4e9d902e3177c2915128f23c7692360e245d3
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-09-18 01:26:21 +0600

    ad

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -13,6 +13,7 @@ import { roomRoutes } from '../modules/room/room.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 import { studentEnrolledCourseRoutes } from '../modules/studentEnrolledCourse/studentEnrolledCourse.routes';
 import { studentEnrolledCourseMarkRoutes } from '../modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes';
+import { studentSemesterPaymentRoutes } from '../modules/studentSemesterPayment/studentSemesterPayment.routes';
 
 const router = express.Router();
 
@@ -77,6 +78,10 @@ const moduleRoutes = [
   {
     path: '/student-enrolled-course-marks',
     route: studentEnrolledCourseMarkRoutes,
+  },
+  {
+    path: '/student-semester-payments',
+    route: studentSemesterPaymentRoutes,
   },
 ];
 
